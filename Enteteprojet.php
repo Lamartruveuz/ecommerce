93d2b9db88af6f50cb0edfc471c967ae79ebb43b
Max666 <malmaxdu47@gmail.com>
2018-09-29 16:35:14 +0200

    Header
    Page d'affichage commune à toutes les pages du site.

---
+++ Enteteprojet.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Site_E-commerce.com</title>
+		<link rel="stylesheet" href="Couleurs/Couleurs.css"/>
+
+<!--<form action="SiteSuite.php" method="GET">
+<input type="text" placeholder="Recherches articles" name="Recherche"/>
+<input type="submit" name="Recheche"/>
+</form>-->
+
+	</head>
+
+	<header>
+		<h1 class="TitreSite">Site de E-Commerce</h1>
+	</header>
+
+
+	<footer>
+		<p>
+		Veuillez nous contacter au numéro suivant : XX.XX.XX.XX.XX<br />
+		Adresse mail : marcel.laquiche@gmail.fr<br />
+		</p>
+	</footer>
+</html>
