c99ca368bd6d07c9a63f6da9e38da6bef6959b1e
MathieuGabriel <mathieu.gabriel@isen.yncrea.fr>
2018-10-05 18:03:29 +0200

    conex_inscri

---
+++ connexion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Connexion
+		</title>
+	</head>
+	
+	<?php include 'Enteteprojet.php'?>
+	<body>
+		<br>
+		<div class="Connexion">
+			<div>
+				<label>IDENTIFICATION :</label>
+			</div>
+				<strong>Vous êtes déjà client</strong>
+			<div>
+				<label>Entrez votre adresse email :</label>
+			</div>
+				<input  type="text"/>
+			<div>
+				<label>Entrez votre mot de passe :</label>
+			</div>
+				<input type="password"/>
+			<div>
+				<input type="button" value="Connexion"/>
+			</div>
+			<div>
+				<strong>Vous n'êtes pas encore clients?</strong>
+			</div>
+			<div>
+				<input type="button" value="Inscription">
+			</div>
+		</div>
+	</body>
+	<?php include 'footer.php'?>
+</html>
---
+++ inscription.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscription</title>
+</head>
+<?php include 'Enteteprojet.php'?>
+<body>
+	<br>
+	<div class="Inscription">
+		<div>
+			<label>CRÉATION DE COMPTE :</label>
+		</div>			
+		<div>			
+			<label>Entrez votre adresse email :</label>
+		</div>
+		<div>
+			<input type="text"/>
+		</div>
+		<div>
+			<label>Confirmez votre email :</label><br />
+		</div>
+		<div>
+			<input type="text"/>
+		</div>			
+		<div>
+			<label>Votre mot de passe :</label>
+			<input type="password"/>
+		</div>
+		<div>
+			<label>Confirmation :</label>
+			<input type="password"/>		
+		</div>
+		<div>
+				<input type="button" value="Inscription">
+		</div>
+	</div>
+</body>
+<?php include 'footer.php'?>
+</html>
