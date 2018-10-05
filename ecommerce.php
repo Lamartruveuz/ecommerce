94265775fc36ec0c605bb25ec168c87863e788e9
Unknown <ALEXANDRE.CANTON@NEUF.FR>
2018-10-05 13:46:08 +0200

    Product page

---
+++ ecommerce.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eat.com</title>
+    <link rel="stylesheet" href="css/common.css" />
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+	
+    
+	<?php include 'Enteteprojet.php' ?>
+
+    <section>
+		<h1>
+			<span class="fn titre_court">Titre court du produit</span>
+
+			<span class="titre_long">Titre détaillé du produit</span>
+			
+		</h1>
+		<aside>	
+		<div>
+    		Prix: <hr> 19.99€
+    	    	<hr>Quantité:<hr>1<hr>
+				<IMG class="displayed" src="240_F_26124444_6ExT1DaHIGJZmIpnKJSv2GJID7QUio1e.jpg"style="width:175px;height:55px;"/>
+
+		</div>
+
+	</aside>
+    	<section>
+			<img id="recipeImage" src="LD0004456449_2.jpg"/>
+			<div class="content">
+    		
+				<h1>Presentation</h1>
+				<p>
+					bla bla bla bla bla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla blabla bla bla bla
+				</p>
+    		
+			</div>
+
+		</section>
+		
+		<div style="float: left; text-align: left">
+Description détaillée du produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit produit
+</div> 
+
+
+
+    
+</body>
+</html>
