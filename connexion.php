2b50a5f0714a32f8efb399947f0de54d818022dc
MathieuGabriel <mathieu.gabriel@isen.yncrea.fr>
2018-10-10 15:23:42 +0200

    Auto stash before merge of "Alex" and "origin/Alex"

--- connexion.php
+++ connexion.php
@@ -10,29 +10,37 @@
 	<?php include 'Enteteprojet.php'?>
 	<body>
 		<br>
-		<div class="Connexion">
+		<br>
+		<br>
+		<main>
+		<br>
+		<div class="Inscri_Connex">
 			<div>
 				<label>IDENTIFICATION :</label>
 			</div>
-				<strong>Vous êtes déjà client</strong>
+			<br>
+				<strong>Vous êtes déjà client ?</strong>
 			<div>
 				<label>Entrez votre adresse email :</label>
 			</div>
-				<input  type="text"/>
+				<input  type="text" placeholder="jean.dupond@gmail"/>
 			<div>
 				<label>Entrez votre mot de passe :</label>
 			</div>
-				<input type="password"/>
+				<input type="password" placeholder="mot de passe"/>
 			<div>
 				<input type="button" value="Connexion"/>
 			</div>
+			<br>
 			<div>
 				<strong>Vous n'êtes pas encore clients?</strong>
 			</div>
 			<div>
 				<input type="button" value="Inscription">
 			</div>
+			<br>
 		</div>
+	</main>
 	</body>
 	<?php include 'footer.php'?>
 </html>
--- inscription.php
+++ inscription.php
@@ -7,34 +7,60 @@
 <?php include 'Enteteprojet.php'?>
 <body>
 	<br>
-	<div class="Inscription">
-		<div>
-			<label>CRÉATION DE COMPTE :</label>
-		</div>			
-		<div>			
-			<label>Entrez votre adresse email :</label>
-		</div>
-		<div>
+	<br>
+	<br>
+	<main>
+	<br>
+	<div class="Inscri_Connex">
+		<p>
+			<label>  CRÉATION DE COMPTE :</label>		
+		</p>
+		<div>	
+			<label>  Entrez votre adresse email :</label><br>
 			<input type="text"/>
 		</div>
+		<p></p>
 		<div>
-			<label>Confirmez votre email :</label><br />
-		</div>
-		<div>
+			<label>  Confirmez votre email :</label><br>
 			<input type="text"/>
-		</div>			
+		</div>	
+		<p></p>		
 		<div>
-			<label>Votre mot de passe :</label>
+			<label>  Votre mot de passe :</label><br>
 			<input type="password"/>
 		</div>
+		<p></p>
 		<div>
-			<label>Confirmation :</label>
+			<label>  Confirmation :</label><br>
 			<input type="password"/>		
 		</div>
+		<p></p>
+		<div>
+			<label>  Adresse livraison :</label><br>
+			<input type="texte"/>
+		</div>
+		<p></p>
+		<div>			
+			<label>  Adresse facturation :</label><br>
+			<input type="texte"/>
+		</div>
+		<p></p>
+		<div>
+			<label>  Nom :</label><br>
+			<input type="texte"/>
+		</div>
+		<p></p>
+		<div>
+			<label>  Prenom :</label><br>
+			<input type="texte"/>
+		</div>
+		<p></p>
 		<div>
 				<input type="button" value="Inscription">
 		</div>
+		<br>
 	</div>
+</main>
 </body>
 <?php include 'footer.php'?>
 </html>
